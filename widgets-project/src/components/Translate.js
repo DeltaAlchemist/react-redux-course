630758d3ba054d4a4aa44655d08d6097d62cabee
Henrique Campos <delta.alchemist@gmail.com>
2021-10-06 21:03:26 -0300

    feat: convert component

--- widgets-project/src/components/Translate.js
+++ widgets-project/src/components/Translate.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
+import Convert from "./Convert";
 
 const options = [
     {
@@ -13,7 +14,20 @@ const options = [
     {
         label: 'Hindi',
         value: 'hi'
-    }
+    },
+    {
+        label: 'Dutch',
+        value: 'nl'
+    },
+    {
+        label: 'Portuguese',
+        value: 'pt'
+    },
+    {
+        label: 'Japanese',
+        value: 'ja'
+    },
+
 ]
 
 const Translate = () => {
@@ -40,6 +54,9 @@ const Translate = () => {
                 selected={language} 
                 onSelectedChange={setLanguage}
             />
+            <div className="ui section divider" />
+            <h3 className="ui header">Output</h3>
+            <Convert text={text} language={language}/>
         </div>
     )
 }

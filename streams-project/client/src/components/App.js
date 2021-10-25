6ed8e34921ee37bc0a53c41f2a51fe9c8fc7b672
Henrique Benicio Campos Silva <henrique_benicio@cargill.com>
2021-10-25 17:36:55 -0300

    feat: add comp boilerplates and routs

--- streams-project/client/src/components/App.js
+++ streams-project/client/src/components/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-const PageOne = () => {
-    return <div>Page One <br />
-        <Link to="/pagetwo">Navigate to Page 2</Link>
-    </div>
-}
-
-const PageTwo = () => {
-    return <div>
-        Page Two <br />
-        <Link to="/">Navigate to Page 1</Link>
-    </div>
-}
+import StreamCreate from './streams/StreamCreate'
+import StreamEdit from './streams/StreamEdit'
+import StreamDelete from './streams/StreamDelete'
+import StreamList from './streams/StreamList'
+import StreamShow from './streams/StreamShow'
 
 
 const App = () => {
     return(
         <div>
             <BrowserRouter>
-                <Route path="/" exact component={PageOne} />
-                <Route path="/pagetwo" component={PageTwo} />
+                <Route path="/" exact component={StreamList} />
+                <Route path="/streams/new" component={StreamCreate} />
+                <Route path="/streams/edit" component={StreamEdit} />
+                <Route path="/streams/delete" component={StreamDelete} />
+                <Route path="/streams/show" component={StreamShow} />
             </BrowserRouter>
         </div>
     )

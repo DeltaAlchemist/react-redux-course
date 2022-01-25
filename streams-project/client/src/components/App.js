ee647ec4abefe55a07c15d31aa3686e70b172689
Henrique Campos <delta.alchemist@gmail.com>
2022-01-25 14:10:03 -0300

    redone last commit

--- streams-project/client/src/components/App.js
+++ streams-project/client/src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Router, Route } from 'react-router-dom';
-import StreamCreate from './Streams/StreamCreate';
-import StreamEdit from './Streams/StreamEdit';
-import StreamDelete from './Streams/StreamDelete';
-import StreamList from './Streams/StreamList';
-import StreamShow from './Streams/StreamShow';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 import { Header } from './Header';
 import history from '../redux/history';
 

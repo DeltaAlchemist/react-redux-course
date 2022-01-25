dc9db9205f9848ea787f4f6da9a9b24e5fc10262
Henrique Campos <delta.alchemist@gmail.com>
2022-01-25 14:02:44 -0300

    style: change streams' folder name

--- streams-project/client/src/components/App.js
+++ streams-project/client/src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Router, Route } from 'react-router-dom';
-import StreamCreate from './streams/StreamCreate';
-import StreamEdit from './streams/StreamEdit';
-import StreamDelete from './streams/StreamDelete';
-import StreamList from './streams/StreamList';
-import StreamShow from './streams/StreamShow';
+import StreamCreate from './Streams/StreamCreate';
+import StreamEdit from './Streams/StreamEdit';
+import StreamDelete from './Streams/StreamDelete';
+import StreamList from './Streams/StreamList';
+import StreamShow from './Streams/StreamShow';
 import { Header } from './Header';
 import history from '../redux/history';
 
@@ -15,10 +15,10 @@ const App = () => {
             <Router history={history}>
                 <Header />
                 <Route path="/" exact component={StreamList} />
-                <Route path="/streams/new" exact component={StreamCreate} />
-                <Route path="/streams/edit" exact component={StreamEdit} />
-                <Route path="/streams/delete" exact component={StreamDelete} />
-                <Route path="/streams/show" exact component={StreamShow} />
+                <Route path="/Streams/new" exact component={StreamCreate} />
+                <Route path="/Streams/edit" exact component={StreamEdit} />
+                <Route path="/Streams/delete" exact component={StreamDelete} />
+                <Route path="/Streams/show" exact component={StreamShow} />
             </Router>
         </div>
     )

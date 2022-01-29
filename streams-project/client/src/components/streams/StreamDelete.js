71b1a651f89c2d1c5cb4ccac4580c14ea28008ca
Henrique Campos <delta.alchemist@gmail.com>
2022-01-28 22:41:54 -0300

    feat: add delete stream modal boilerplate

--- streams-project/client/src/components/streams/StreamDelete.js
+++ streams-project/client/src/components/streams/StreamDelete.js
@@ -1,6 +1,27 @@
+import React from "react";
+import { Modal } from "../Modal";
+import history from "../../redux/history";
+
 const StreamDelete = () => {
+
+    const actions = (
+        // we could use <> or <React.Fragment>
+        <React.Fragment>
+            <button className="ui button negative">Delete</button>
+            <button className="ui button">Cancel</button>
+        </React.Fragment>
+    );
+
     return(
-        <div>StreamDelete</div>
+        <div>
+            StreamDelete
+            <Modal 
+                title="Delete stream"
+                content="Are you sure you want to delete this stream?"
+                actions={actions}
+                onDismiss={() => history.push("/")}
+            />
+        </div>
     );
 }
 

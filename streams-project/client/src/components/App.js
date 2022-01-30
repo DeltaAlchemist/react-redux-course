562adc5a96bd7fd54a014e6d10381402ca5597a6
Henrique Campos <delta.alchemist@gmail.com>
2022-01-30 16:34:31 -0300

    feat: link stream show

--- streams-project/client/src/components/App.js
+++ streams-project/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
@@ -10,18 +10,20 @@ import history from '../redux/history';
 
 
 const App = () => {
-    return(
-        <div className="ui container">
-            <Router history={history}>
-                <Header />
-                <Route path="/" exact component={StreamList} />
-                <Route path="/Streams/new" exact component={StreamCreate} />
-                <Route path="/Streams/edit/:id" exact component={StreamEdit} />
-                <Route path="/Streams/delete/:id" exact component={StreamDelete} />
-                <Route path="/Streams/show" exact component={StreamShow} />
-            </Router>
-        </div>
-    )
+    return (
+      <div className="ui container">
+        <Router history={history}>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+          </Switch>
+        </Router>
+      </div>
+    );
 }
 
 export default App

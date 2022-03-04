ab276508cac8ad420f572ed1196958ff1f9382f2
Henrique Campos <delta.alchemist@gmail.com>
2022-03-03 21:48:44 -0300

    feat: add language store

--- translation-project/src/contexts/LanguageContext.js
+++ translation-project/src/contexts/LanguageContext.js
@@ -1,3 +1,21 @@
 import React from "react";
 
-export default React.createContext('en');
+const Context = React.createContext('en');
+
+export class LanguageStore extends React.Component {
+  state = { language: 'en' };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  }
+
+  render() {
+    return (
+      <Context.Provider value={{ ...this.state, onLanguageChange }}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export default Context;
